8d305b802a043e5d6b44cd76b637887282449c07
wterroni <wellington.terroni@outlook.com>
2019-01-25 13:17:23 -0200

    Inclui login e criação de novo usuario

--- app/src/main/java/com/ifcapp/ifcapp/activity/LoginActivity.kt
+++ app/src/main/java/com/ifcapp/ifcapp/activity/LoginActivity.kt
@@ -7,13 +7,19 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.view.View
 import android.widget.Toast
 import com.ifcapp.ifcapp.R
 import com.ifcapp.ifcapp.Util.Util
 import kotlinx.android.synthetic.main.activity_login.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    private var user: FirebaseUser? = null
+    private var novoUsuario = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,29 +27,89 @@ class LoginActivity : AppCompatActivity() {
         initialize()
     }
 
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth?.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            startMainActivity()
+        }
+    }
 
     private fun initialize() {
+        mAuth = FirebaseAuth.getInstance()
         setNotification()
-        var buttonEntrar = findViewById(R.id.btnEntrar) as Button
-        buttonEntrar.setOnClickListener {
-            checkLoginIsTrue();
-        }
+        setNovoUsuario()
+        setButton()
         Util.openKeyboard(this, editTextSenha)
     }
 
-    private fun checkLoginIsTrue() {
-        var cpf = edtCpfCnpj.text.toString().replace(".", "").replace("-", "");
-        if (cpf == "") {
-            Toast.makeText(this, "CPF ou Senha inválidos.", Toast.LENGTH_LONG).show();
-        } else {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent);
+    private fun setButton() {
+        btnEntrar.setOnClickListener {
+            loginAction();
         }
+    }
+
+    private fun setNovoUsuario() {
+        novoUsuarioTextView.setOnClickListener {
+            btnEntrar.text = getString(R.string.criar)
+            novoUsuarioTextView.visibility = View.GONE
+            novoUsuario = true
+        }
+    }
+
+    private fun loginAction() {
+        if (novoUsuario) {
+            criaUsuario()
+        } else {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        mAuth?.signInWithEmailAndPassword(emailEditText.text.toString(),
+                editTextSenha.text.toString())
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, getString(R.string.msg_login_sucesso))
+                        val user = mAuth?.getCurrentUser()
+                        updateUI(user)
+                    } else {
+                        Log.w(TAG, getString(R.string.msg_login_falha), task.exception)
+                        Toast.makeText(this, getString(R.string.msg_login_falha),
+                                Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+    }
+
+    private fun criaUsuario() {
+        mAuth?.createUserWithEmailAndPassword(emailEditText.text.toString(),
+                editTextSenha.text.toString())
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, getString(R.string.msg_novo_usuario_sucesso))
+                        user = mAuth?.getCurrentUser()
+                        updateUI(user)
+                    } else {
+                        Log.w(TAG, getString(R.string.msg_novo_usuario_falha), task.exception)
+                        Toast.makeText(this, getString(R.string.msg_novo_usuario_falha),
+                                Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent);
     }
 
     private fun setNotification() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create channel to show notifications.
             val channelId = getString(R.string.default_notification_channel_id)
             val channelName = getString(R.string.default_notification_channel_name)
             val notificationManager = getSystemService(NotificationManager::class.java)
@@ -51,27 +117,17 @@ class LoginActivity : AppCompatActivity() {
                     channelName, NotificationManager.IMPORTANCE_LOW))
         }
 
-        // If a notification message is tapped, any data accompanying the notification
-        // message is available in the intent extras. In this sample the launcher
-        // intent is fired when the notification is tapped, so any accompanying data would
-        // be handled here. If you want a different intent fired, set the click_action
-        // field of the notification message to the desired intent. The launcher intent
-        // is used when no click_action is specified.
-        //
-        // Handle possible data accompanying notification message.
-        // [START handle_data_extras]
         intent.extras?.let {
             for (key in it.keySet()) {
                 val value = intent.extras.get(key)
                 Log.d(TAG, "Key: $key Value: $value")
             }
         }
-
     }
 
     companion object {
 
-        private const val TAG = "MainActivity"
+        private const val TAG = "LoginActivity"
     }
 }
 
